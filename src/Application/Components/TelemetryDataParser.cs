644f8610daceb6dca9246b84e137b0b7834f2da9
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2021-04-22 08:10:05 +0200

    Package/-s updated to most recent version

--- src/Application/Components/TelemetryDataParser.cs
+++ src/Application/Components/TelemetryDataParser.cs
@@ -7,13 +7,16 @@ using Aspenlaub.Net.GitHub.CSharp.Wakek.Entities;
 using Aspenlaub.Net.GitHub.CSharp.Wakek.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.Wakek.Interfaces.Components;
 using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace Aspenlaub.Net.GitHub.CSharp.Wakek.Application.Components {
     public class TelemetryDataParser : ITelemetryDataParser {
         public bool TryParse(string s, out IList<ITelemetryData> result) {
             try {
-                var csvReader = new CsvReader(new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(s))), CultureInfo.CurrentCulture);
-                csvReader.Configuration.Delimiter = ";";
+                var configuration = new CsvConfiguration(CultureInfo.CurrentCulture) {
+                    Delimiter = ";"
+                };
+                var csvReader = new CsvReader(new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(s))), configuration);
                 result = csvReader.GetRecords<TelemetryData>().Cast<ITelemetryData>().ToList();
                 return true;
             } catch {
