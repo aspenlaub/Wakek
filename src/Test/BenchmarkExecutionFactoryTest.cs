84bdc9ad1f1485d3b9b2ff247ff42343dc1edb17
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2025-10-28 15:48:43 +0100

    Package/-s updated to most recent version

--- src/Test/BenchmarkExecutionFactoryTest.cs
+++ src/Test/BenchmarkExecutionFactoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Aspenlaub.Net.GitHub.CSharp.Pegh.Entities;
 using Aspenlaub.Net.GitHub.CSharp.Wakek.Application.Components;
 using Aspenlaub.Net.GitHub.CSharp.Wakek.Interfaces;
 using Aspenlaub.Net.GitHub.CSharp.Wakek.Interfaces.Components;
@@ -10,25 +11,25 @@ namespace Aspenlaub.Net.GitHub.CSharp.Wakek.Test;
 
 [TestClass]
 public class BenchmarkExecutionFactoryTest {
-    private readonly IContainer Container;
+    private readonly IContainer _Container = new ContainerBuilder().UseWakek().Build();
 
-    public BenchmarkExecutionFactoryTest() {
-        Container = new ContainerBuilder().UseWakek().Build();
-    }
-
-    [TestMethod, ExpectedException(typeof(NullReferenceException))]
+    [TestMethod]
     public void NeedBenchmarkDefinitionGuidForCreationOfExecution() {
         var benchmarkDefinitionMock = new Mock<IBenchmarkDefinition>();
         benchmarkDefinitionMock.SetupGet(b => b.Guid).Returns("");
-        var sut = new BenchmarkExecutionFactory(Container.Resolve<ISequenceNumberGenerator>());
-        sut.CreateBenchmarkExecution(benchmarkDefinitionMock.Object);
+        var sut = new BenchmarkExecutionFactory(_Container.Resolve<ISequenceNumberGenerator>());
+        Assert.ThrowsExactly<NullReferenceException>(() => {
+            sut.CreateBenchmarkExecution(benchmarkDefinitionMock.Object);
+        }, "", "");
     }
 
-    [TestMethod, ExpectedException(typeof(NullReferenceException))]
+    [TestMethod]
     public void NeedBenchmarkExecutionGuidForCreationOfState() {
         var benchmarkExecutionMock = new Mock<IBenchmarkExecution>();
         benchmarkExecutionMock.SetupGet(b => b.Guid).Returns("");
-        var sut = new BenchmarkExecutionFactory(Container.Resolve<ISequenceNumberGenerator>());
-        sut.CreateBenchmarkExecutionState(benchmarkExecutionMock.Object, 1);
+        var sut = new BenchmarkExecutionFactory(_Container.Resolve<ISequenceNumberGenerator>());
+        Assert.ThrowsExactly<NullReferenceException>(() => {
+            sut.CreateBenchmarkExecutionState(benchmarkExecutionMock.Object, 1);
+        }, "", "");
     }
 }
